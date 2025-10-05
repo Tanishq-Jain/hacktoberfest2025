88327a0e9e43cb7e986d01d0a12ccb1e41e67c35
Mr.Incredible.29 <65172813+Naitik-Jain@users.noreply.github.com>
2025-10-05 21:56:15 +0530

    Revert "Update insertion sort.c"

--- insertion sort.c
+++ insertion sort.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 
-int main() {
-    int n;
-    printf("Insertion Sort\nEnter number of elements: ");
-    scanf("%d", &n);
+// Function to perform Insertion Sort
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
 
-    int arr[n];
-    printf("Enter elements: ");
-    for(int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    // Insertion Sort
-    for(int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        // Move elements greater than key to one position ahead
-        while(j >= 0 && arr[j] > key) {
+        // Move elements greater than key one position ahead
+        while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j--;
+            j = j - 1;
         }
         arr[j + 1] = key;
     }
+}
 
-    printf("Sorted array: ");
-    for(int i = 0; i < n; i++)
+// Function to print array elements
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
+}
+
+// Main function
+int main() {
+    int arr[] = {23, 4, 4, 2, 5, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
 
     return 0;
 }

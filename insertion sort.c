0537c599409938d1c31cee8b56029bca9645047a
Mr.Incredible.29 <65172813+Naitik-Jain@users.noreply.github.com>
2025-10-05 21:15:17 +0530

    Update insertion sort.c
    user defined approach

--- insertion sort.c
+++ insertion sort.c
@@ -1,40 +1,32 @@
 #include <stdio.h>
 
-// Function to perform Insertion Sort
-void insertionSort(int arr[], int n) {
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
+int main() {
+    int n;
+    printf("Insertion Sort\nEnter number of elements: ");
+    scanf("%d", &n);
 
-        // Move elements greater than key one position ahead
-        while (j >= 0 && arr[j] > key) {
+    int arr[n];
+    printf("Enter elements: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Insertion Sort
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements greater than key to one position ahead
+        while(j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
         arr[j + 1] = key;
     }
-}
-
-// Function to print array elements
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-// Main function
-int main() {
-    int arr[] = {23, 4, 4, 2, 5, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: ");
-    printArray(arr, n);
-
-    insertionSort(arr, n);
 
     printf("Sorted array: ");
-    printArray(arr, n);
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 
     return 0;
 }
